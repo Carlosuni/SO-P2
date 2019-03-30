3af1f12f344194fd48bb910b6f4d64bed5ef66d9
CarlosUni <100074907@alumnos.uc3m.es>
2019-03-30 15:40:37 +0100

    Added tester

---
+++ test_launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "CREANDO ZIP PARA LA ENTREGA"
+cd ssoo_p2
+zip -r ssoo_p2_100074907.zip * 
+
+echo "PASANDO CORRECTOR"
+./corrector_ssoo_p2-v3.sh ssoo_p2_100074907.zip
+
+echo ""
+
